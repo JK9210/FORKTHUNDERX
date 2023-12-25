b79bca006724f44917c2437c389c718e84b35aec
メ ᴋᴜɴᴀʟ 么 <148003454+APL9210@users.noreply.github.com>
2023-12-25 17:06:14 +0530

    Update errors.py

--- SpamX/core/errors.py
+++ SpamX/core/errors.py
@@ -1,4 +1,4 @@
-""" RiZoeLX 2022-2023 © SpamX """
+""" 2024-2025 © THUNDER X """
 
 import re
 
@@ -15,22 +15,22 @@ def user_errors(error):
 
 def join_errors(error):
     if '[400 USERNAME_NOT_OCCUPIED]' in str(error):
-       return "You didn't Provide Username to join!"
+       return "You didn't Provide Username to join! 👀"
     elif '[400 USERNAME_INVALID]' in str(error):
-       return "Ahh.. Username is invalid"
+       return "Ahh.. Username is invalid 🥲"
     elif '[400 INVITE_REQUEST_SENT]' in str(error):
-       return "Join request sent!"
+       return "Join request sent!✅"
     elif 'Username not found' in str(error):
-       return "I'm banned in that group 🫠"
+       return "I'm banned in that group 🥹"
     else:
        return f"**Unknown Error:** \n\n {error}" 
 
 def leave_errors(error):
     if '[400 PEER_ID_INVALID]' in str(error):
-       return "Wrong Chat ID!"
+       return "Wrong Chat ID! 😮‍💨"
     elif '[400 USER_NOT_PARTICIPANT]' in str(error):
-       return "I'm not in that group 🫠"
+       return "I'm not in that group ❌"
     elif '[400 USERNAME_INVALID]' in str(error):
-       return "Ahh.. Username is invalid"
+       return "Ahh.. Username is invalid 🤧"
     else:
        return f"**Unknown Error:** \n\n {error}" 
