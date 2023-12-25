d6f0338a855c37529a3786a60656297c2ad68884
メ ᴋᴜɴᴀʟ 么 <148003454+APL9210@users.noreply.github.com>
2023-12-25 17:16:22 +0530

    Update run_clients.py

--- SpamX/core/run_clients.py
+++ SpamX/core/run_clients.py
@@ -15,126 +15,126 @@ from pyrogram import idle
 def Run_SpamX():
     if CLIENT:
        if ":" in CLIENT:
-         start_spamX(RiZoeL, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x, "token")
        else:
-         start_spamX(RiZoeL, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x, "session")
 
     if CLIENT2:
        if ":" in CLIENT2:
-         start_spamX(RiZoeL2, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x2, "token")
        else:
-         start_spamX(RiZoeL2, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x2, "session")
          
     if CLIENT3:
        if ":" in CLIENT3:
-         start_spamX(RiZoeL3, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x3, "token")
        else:
-         start_spamX(RiZoeL3, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x3, "session")
          
     if CLIENT4:
        if ":" in CLIENT4:
-         start_spamX(RiZoeL4, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x4, "token")
        else:
-         start_spamX(RiZoeL4, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x4, "session")
          
     if CLIENT5:
        if ":" in CLIENT5:
-         start_spamX(RiZoeL5, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x5, "token")
        else:
-         start_spamX(RiZoeL5, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x5, "session")
          
     if CLIENT6:
        if ":" in CLIENT6:
-         start_spamX(RiZoeL6, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x6, "token")
        else:
-         start_spamX(RiZoeL6, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x6, "session")
          
     if CLIENT7:
        if ":" in CLIENT7:
-         start_spamX(RiZoeL7, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x7, "token")
        else:
-         start_spamX(RiZoeL7, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x7, "session")
          
     if CLIENT8:
        if ":" in CLIENT8:
-         start_spamX(RiZoeL8, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x8, "token")
        else:
-         start_spamX(RiZoeL8, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x8, "session")
          
     if CLIENT9:
        if ":" in CLIENT9:
-         start_spamX(RiZoeL9, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x9, "token")
        else:
-         start_spamX(RiZoeL9, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x9, "session")
 
     if CLIENT10:
        if ":" in CLIENT10:
-         start_spamX(RiZoeL10, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x10, "token")
        else:
-         start_spamX(RiZoeL10, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x10, "session")
          
     if CLIENT11:
        if ":" in CLIENT11:
-         start_spamX(RiZoeL11, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x11, "token")
        else:
-         start_spamX(RiZoeL11, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x11, "session")
          
     if CLIENT12:
        if ":" in CLIENT12:
-         start_spamX(RiZoeL12, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x12, "token")
        else:
-         start_spamX(RiZoeL12, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x12, "session")
          
     if CLIENT13:
        if ":" in CLIENT13:
-         start_spamX(RiZoeL13, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x13, "token")
        else:
-         start_spamX(RiZoeL13, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x13, "session")
          
     if CLIENT14:
        if ":" in CLIENT14:
-         start_spamX(RiZoeL14, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x14, "token")
        else:
-         start_spamX(RiZoeL14, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x14, "session")
          
     if CLIENT15:
        if ":" in CLIENT15:
-         start_spamX(RiZoeL15, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x15, "token")
        else:
-         start_spamX(RiZoeL15, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x15, "session")
          
     if CLIENT16:
        if ":" in CLIENT16:
-         start_spamX(RiZoeL16, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x16, "token")
        else:
-         start_spamX(RiZoeL16, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x16, "session")
          
     if CLIENT17:
        if ":" in CLIENT17:
-         start_spamX(RiZoeL17, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x17, "token")
        else:
-         start_spamX(RiZoeL17, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x17, "session")
          
     if CLIENT18:
        if ":" in CLIENT18:
-         start_spamX(RiZoeL18, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x18, "token")
        else:
-         start_spamX(RiZoeL18, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x18, "session")
          
     if CLIENT19:
        if ":" in CLIENT19:
-         start_spamX(RiZoeL19, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x19, "token")
        else:
-         start_spamX(RiZoeL19, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x19, "session")
 
     if CLIENT20:
        if ":" in CLIENT20:
-         start_spamX(RiZoeL20, "token")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x20, "token")
        else:
-         start_spamX(RiZoeL20, "session")
+         start_spamX(ᴛʜᴜɴᴅᴇʀ x20, "session")
     
-    print(f"SpamX - [INFO]: Python Version - {platform.python_version()}")
-    print(f"SpamX - [INFO]: THUNDERX Version - {__version__}")
+    print(f"ThunderX - [INFO]: Python Version - {platform.python_version()}")
+    print(f"ThunderX - [INFO]: THUNDERX Version - {__version__}")
     print(""" \n\n
      ╒═══════════════════════════╕
     ʏᴏᴜʀ ᴛʜᴜɴᴅᴇʀ x ʜᴀꜱ ʙᴇᴇɴ ᴅᴇᴘʟᴏʏᴇᴅ!!
